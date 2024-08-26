ca19f43761878ad26362daa173a853fee7ab37c7
Elderthemarvel <32645126+Elderthemarvel@users.noreply.github.com>
2024-08-25 22:10:58 -0600

    comentario

--- app/Views/home.php
+++ app/Views/home.php
@@ -8,6 +8,7 @@
         <h4 class="card-subtitle mb-4 text-center text-muted">Control de ciudadanos</h4>
         <p class="card-text"><strong>Estudiante:</strong> Elder Alejandro Gonzalez Guzman</p>
         <p class="card-text"><strong>Número de carnet:</strong> 201016328</p>
+        <p class="card-text">Se realizaron modificaciones a la base de datos para evitar errores en en las claves primarias a todas se les agrego autoincrement excepto a la de ciudadanos</p>
     </div>
 </div>
 <?= $this->endSection()?>
